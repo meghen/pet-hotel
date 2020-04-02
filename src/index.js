3fea33f96138416668be0a87b661de297a07a02b
Derek Boat <derek.boat@gmail.com>
2020-04-02 14:29:26 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
-import * as serviceWorker from './serviceWorker';
-=======
-import App from './App';
+
+
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
