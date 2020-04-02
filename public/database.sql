74fb3098b21e5e6ee86c7a4c1980191ce5639a31
Derek Boat <derek.boat@gmail.com>
2020-04-02 13:51:37 -0500

    set up database.sql file

---
+++ public/database.sql
@@ -0,0 +1,23 @@
+
+
+--Create database named "pet_hotel"
+CREATE TABLE "owners" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) NOT NULL
+);
+
+CREATE TABLE "pets" (
+    "id" SERIAL PRIMARY KEY,
+    "owner_id" INTEGER REFERENCES "owners",
+    "pet" VARCHAR (50),
+    "breed" VARCHAR (50),
+    "color" VARCHAR (50),
+    "checked_in" VARCHAR (50)
+);
+
+--Hard Coded data 
+
+INSERT INTO "owners" ("name") VALUES ('Chris'), ('Ally'), ('Dane');
+
+INSERT INTO "pets" ("owner_id", "pet", "breed", "color", "checked_in") VALUES ('1', 'Charlie', 'Shih-tzu', 'black', 'no')
+,('1', 'Thorin', 'Rabbit', 'White', 'no'), ('2', 'Gatsby', 'Cat', 'White', '5/5/18'), ('3', 'Juniper', 'Cat', 'tabby', 'no');
